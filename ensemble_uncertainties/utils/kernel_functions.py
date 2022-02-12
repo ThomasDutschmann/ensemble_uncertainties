e4b23c8f9376e0d0424852597c284908e290e3b0
ThomasDutschmann <thomasdutschmann@gmail.com>
2022-02-12 17:43:47 +0100

    Added Tanimoto kernel function

---
+++ ensemble_uncertainties/utils/kernel_functions.py
@@ -0,0 +1,27 @@
+
+"""Library of custom kernel functions for SVM."""
+
+import numpy as np
+
+from sklearn.metrics import pairwise_distances
+
+
+def tanimoto(A, B):
+    """Fast implementation of the Tanimoto similarity.
+
+    Parameters
+    ----------
+    A : list-like
+        The first bit-sequence
+    B : list-like
+        The second bit-sequence
+    
+    Returns
+    -------
+    float
+        Tanimoto similarity (\in [0, 1])
+    """
+    def metric(a, b):
+        ab = np.sum((a + b) == 2)
+        return ab / (np.sum(a) + np.sum(b) - ab)
+    return pairwise_distances(A, B, metric=metric)
