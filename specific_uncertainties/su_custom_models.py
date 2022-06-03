69bd88321204d3290aafb153ed822983349f832c
ThomasDutschmann <thomasdutschmann@gmail.com>
2022-06-03 06:35:08 +0200

    Added generic k-fold ensemble UQ

--- specific_uncertainties/su_custom_models.py
+++ specific_uncertainties/su_custom_models.py
@@ -1,8 +1,17 @@
 
 """Wrapper classes for regressors that can be tuned to yield uncertainties."""
 
+from ensemble_uncertainties.constants import V_THRESHOLD
 import numpy as np
+import pandas as pd
 
+from ensemble_uncertainties.automatize import run_evaluation
+from ensemble_uncertainties.constants import (
+    N_REPS,
+    N_SPLITS,
+    RANDOM_SEED,
+    V_THRESHOLD
+)
 from ensemble_uncertainties.neural_estimators.neural_estimator import (
     DeepMCDropoutRegressor
 )
@@ -75,3 +84,129 @@ class UQRandomForestRegressor(RandomForestRegressor):
         dt_preds = np.array([tree.predict(X) for tree in trees])
         sdevs = dt_preds.std(axis=0)
         return predictions, sdevs
+
+
+class KFoldEnsembleRegressorWrapper():
+    """Wrapper that turns every regressor into a k-fold-ensemble predictor."""
+
+    def __init__(self, model, **evaluator_kwargs):
+        """Initializer.
+
+        Parameters
+        ----------
+        model : object
+            A fittable regressor object, e.g. sklearn.svm.SVR()
+            that implements the methods fit(X, y) and predict(X)
+        **evaluator_kwargs
+            Arguments that are forwarded to automatize.run_evaluation(...).
+        """
+        self.model = model
+        self.evaluator_kwargs = evaluator_kwargs
+
+    def custom_follow_up(self, evaluator):
+        """Incorporates the evaluator-object from
+        the evaluation-run as attribute.
+
+        Parameters
+        ----------
+        evaluator : Evaluator
+            Evaluator-object for which 'perform(...)' has been run
+        """
+        self.performed_evaluator = evaluator
+
+    def fit(self, X, y):
+        """Fits the underlying evaluator regressor.
+        
+        Parameters
+        ----------
+        X : DataFrame
+            Train inputs
+        y : DataFrame
+            Train outputs
+        """
+        # Make y to DataFrame
+        y_df = pd.DataFrame(X.index)
+        y_df['y'] = y
+        y_df.set_index('id', inplace=True)
+        # Perform evaluator
+        run_evaluation(
+            model=self.model,
+            task='regression',
+            X=X,
+            y=y_df,
+            verbose=False,
+            repetitions=self.evaluator_kwargs['repetitions'],
+            n_splits=self.evaluator_kwargs['n_splits'],
+            seed=self.evaluator_kwargs['seed'],
+            scale=self.evaluator_kwargs['scale'], 
+            v_threshold=self.evaluator_kwargs['v_threshold'],
+            bootstrapping=False,
+            path=None,
+            store_all=False,
+            args=None,
+            follow_up=self.custom_follow_up
+        )
+
+    def uq_predict(self, X):
+        """Predicts the output of X by every
+        model-set in the performed evaluator.
+        
+        Parameters
+        ----------
+        X : DataFrame
+            Test set to predict
+            
+        Returns
+        -------
+        np.array, np.array
+            Predictions, uncertainties        
+        """
+        # Initialize
+        evaluator = self.performed_evaluator
+        predictions_df = pd.DataFrame(index=X.index)
+        # Go through each iteration of the evaluator
+        for rep_index in range(evaluator.repetitions):
+            for split_index in range(evaluator.n_splits):
+                column_name = f'{rep_index}_{split_index}'
+                # Get current scaler, threshold, and model
+                scaler = evaluator.scalers[rep_index][split_index]
+                vt = evaluator.vt_filters[rep_index][split_index]
+                model = evaluator.models[rep_index][split_index]
+                # Scale/filter test inputs
+                X_te_sc = pd.DataFrame(scaler.transform(X),
+                    index=X.index, columns=X.columns)
+                X_te_sc_filt = pd.DataFrame(X_te_sc,
+                    index=X.index, columns=X.columns[vt.get_support()])
+                # Predict
+                predictions = model.predict(X_te_sc_filt)
+                predictions_df[column_name] = predictions
+        # Store latest predictions as attribute
+        self.last_predictions = predictions_df
+        # Return requested values
+        mean = predictions_df.mean(axis=1).values
+        sdevs = predictions_df.std(axis=1).values
+        return mean, sdevs
+
+    def RFRWrapper():
+        """Returns wrapped sklearn RandomForestRegressor."""
+        regressor = KFoldEnsembleRegressorWrapper(
+            model=RandomForestRegressor(),
+            **{'repetitions': N_REPS,
+            'n_splits': N_SPLITS,
+            'seed': RANDOM_SEED,
+            'scale': False, 
+            'v_threshold': V_THRESHOLD}
+        )
+        return regressor
+
+    def MCDropoutWrapper():
+        """Returns wrapped MC Dropout Regressor from neural_estimators."""
+        regressor = KFoldEnsembleRegressorWrapper(
+            model=DeepMCDropoutRegressor(),
+            **{'repetitions': N_REPS,
+            'n_splits': N_SPLITS,
+            'seed': RANDOM_SEED,
+            'scale': True, 
+            'v_threshold': V_THRESHOLD}
+        )
+        return regressor
--- specific_uncertainties/su_model_library.py
+++ specific_uncertainties/su_model_library.py
@@ -4,6 +4,7 @@ provide uncertainties when predicting.
 """
 
 from su_custom_models import (
+    KFoldEnsembleRegressorWrapper,
     MCDropoutRegressorWrapper,
     UQRandomForestRegressor
 )
@@ -13,3 +14,5 @@ from su_custom_models import (
 models = dict()
 models['rf'] = UQRandomForestRegressor()
 models['mc_dropout'] = MCDropoutRegressorWrapper()
+models['ensemble_rf'] = KFoldEnsembleRegressorWrapper.RFRWrapper()
+models['ensemble_dropout'] = KFoldEnsembleRegressorWrapper.MCDropoutWrapper()
