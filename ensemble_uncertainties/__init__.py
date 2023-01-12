fe12655f6337724f3a2cd2f37279889151ce84c2
Thomas Dutschmann <t.dutschmann@tu-braunschweig.de>
2023-01-12 13:33:48 +0100

    Extended to provide high-level availability

---
+++ ensemble_uncertainties/__init__.py
@@ -0,0 +1,15 @@
+
+"""Providing high-level availability for most important functionalities"""
+
+from ensemble_uncertainties.automatize import *
+
+from ensemble_uncertainties.evaluators.classification_evaluator import (
+    ClassificationEvaluator
+)
+
+from ensemble_uncertainties.evaluators.regression_evaluator import (
+    RegressionEvaluator
+)
+
+from ensemble_uncertainties.utils.ad_assessment import *
+from ensemble_uncertainties.utils.plotting import *
